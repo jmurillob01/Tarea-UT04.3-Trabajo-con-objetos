159e6396415ecfc5d44634d9a296ce90b698fcde
Javier Murillo <javiermb2302@gmail.com>
2023-01-11 12:03:33 +0100

    Person properties
    Created properties and the first part of the constructor

---
+++ scripts/Person.js
@@ -0,0 +1,16 @@
+"use strict"; 
+
+// Object to identify the data of a person
+class Person{
+
+    // Properties
+    #name;
+    #lastname1;
+    #lastname2;
+    #born;
+    #picture;
+
+    constructor(name, lastname1, lastname2, born, picture){
+
+    }
+}
