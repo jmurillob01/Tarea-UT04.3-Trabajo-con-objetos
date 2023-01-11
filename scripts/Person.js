56120fe2c88efd9d13ffdcd4b8de3402f7a03f9c
Javier Murillo <javiermb2302@gmail.com>
2023-01-11 12:18:51 +0100

    Person properties v2
    Set the property values and comments

--- scripts/Person.js
+++ scripts/Person.js
@@ -12,5 +12,18 @@ class Person{
 
     constructor(name, lastname1, lastname2, born, picture){
 
+        // Control con excepciones
+        // name es obligatorio
+        // lastname1 es obligatorio
+        // lastname2 es opcional
+        // born es obligatorio
+        // picture es opcional
+
+        // Set the property values
+        this.#name = name;
+        this.#lastname1 = lastname1;
+        this.#lastname2 = lastname2;
+        this.#born = born;
+        this.#picture = picture;
     }
 }
