f0bcee01350506079b38aaae1874a13e8aadf8c6
Javier Murillo <javiermb2302@gmail.com>
2023-01-17 20:54:33 +0100

    Serie Optimization

--- scripts/Serie.js
+++ scripts/Serie.js
@@ -1,7 +1,8 @@
 "use strict";
 import Production from "./Production.js";
 import Resource from "./Resource.js";
-import { InvalidProductionInstanceException } from "./Exception.js";
+import { InvalidSerieException } from "./Exception.js";
+import Coordinate from "./Coordinate.js";
 
 class Serie extends Production {
 
@@ -9,17 +10,16 @@ class Serie extends Production {
     #locations = new Array();
     #season;
 
-    // TODO: Crear localización por defecto una vez creada la clase
-    constructor(title, nationality, publication, synopsis, image, resource = new Resource(0, "www.noLink.com"), locations, seasons = 0) {
+    constructor(title, nationality, publication, synopsis, image, resource = new Resource(0, "www.noLink.com"), locations = new Coordinate(0,0), seasons = 0) {
 
         super(title, nationality, publication, synopsis, image);
 
         // Resource check
-        if (!(resource instanceof Resource)) throw new InvalidProductionInstanceException("resource", "Serie");
+        if (!(resource instanceof Resource)) throw new InvalidSerieException("resource");
         // Coordinate check
-        if (!(locations instanceof Coordinate)) throw new InvalidProductionInstanceException("coordinate", "Serie");
+        if (!(locations instanceof Coordinate)) throw new InvalidSerieException("coordinate");
         // Seasons check
-        if (isNaN(seasons) || seasons < 0) throw new InvalidProductionInstanceException("season", "Serie");
+        if (isNaN(seasons) || seasons < 0) throw new InvalidSerieException("season");
 
         this.#resource = resource;
         this.#locations.push(locations);
@@ -28,19 +28,19 @@ class Serie extends Production {
 
     set resource(resource) {
         // Resource check
-        if (!(resource instanceof Resource)) throw new InvalidProductionInstanceException("resource", "Serie");
+        if (!(resource instanceof Resource)) throw new InvalidSerieException("resource");
         this.#resource = resource;
     }
 
     set locations(locations) {
         // Coordinate check
-        if (!(locations instanceof Coordinate)) throw new InvalidProductionInstanceException("coordinate", "Serie");
+        if (!(locations instanceof Coordinate)) throw new InvalidSerieException("coordinate");
         this.#locations.push(locations);
     }
 
     set seasons(season) {
         // Seasons check
-        if (isNaN(seasons) || seasons < 0) throw new InvalidProductionInstanceException("season", "Serie");
+        if (isNaN(seasons) || seasons < 0) throw new InvalidSerieException("season");
         this.#season = Math.round(seasons);
     }
 }
