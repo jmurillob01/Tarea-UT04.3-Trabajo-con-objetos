ff367c3b79815513c7d71e2d8acfcb1745dabb3d
Javier Murillo <javiermb2302@gmail.com>
2023-01-18 13:39:38 +0100

    Data structure

--- scripts/VideoSystem.js
+++ scripts/VideoSystem.js
@@ -4,23 +4,54 @@
 let VideoSystem = (function (){
     let instantiated;
 
-    function init(){ // Inicialización del singleton
+    function init(){ // Singleton initialization
 
         class VideoSystem{
+            #name; // Name of the system
+            #users = []; // Users from the system
+            #productions = []; // Productions from the system
+            #categories = []; // Categories from the system
+            #actors = []; // Actors from the system
+            #director = []; // Directors from the system
 
+            /**
+                Structure to store objects
+               
+                #director[
+                    {
+                        director: person,
+                        productions: [productions] // Array containing productions reference
+                    }
+                ]
+                #actors[
+                    {
+                        actor: person,
+                        productions: [productions] // Array containing productions reference
+                    }
+                ]:
+                #categories[
+                    { 
+						category: category,
+						productions: [productions] // Array containing productions reference
+					}
+                ]
+                #productions: [] // Array containing system productions
+                #users: [] // Array containing system users
+                #name: // String containing name from the system
+             */
         }
 
-        let instace = new VideoSystem(); //Devolvemos el objeto VideoSystem para que sea una instancia única.
+        let instace = new VideoSystem(); // We return the VideoSystem object to be a single instance.
         Object.freeze(instace);
         return instance;
-    } // Fin inicialización del singleton
+    } // End of singleton initialization
     return{
-        // Devuelve un objeto con el método getInstance
+        // Return an object with the getInstance method
 		getInstance: function () {
-			if (!instantiated) { //Si la variable instantiated es undefined, priemera ejecución, ejecuta init.
-				instantiated = init(); //instantiated contiene el objeto único
+			if (!instantiated) { // If the instantiated variable is undefined, run init.
+				instantiated = init(); // Instantiated contains the single object
 			}
-			return instantiated; //Si ya está asignado devuelve la asignación.
+			return instantiated; // If it is already assigned, it returns the assignment.
 		}
     };
 })();
