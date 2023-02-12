7e33d2066aa74ee8c0314a3d15a83a25ec1542cb
Javier Murillo <javiermb2302@gmail.com>
2023-02-12 22:35:22 +0100

    loadVideoSystemObjects method
    Version 1.0

--- scripts/videoSystem/videoSystemApp.js
+++ scripts/videoSystem/videoSystemApp.js
@@ -1,1 +1,16 @@
 "use strict";
+
+import VideoSystem from "./videoSystemModel.js";
+import VideoSystemView from "./videoSystemView.js";
+import VideoSystemController from "./videoSystemController.js";
+
+let VideoSystemApp;
+
+
+(function() {
+	VideoSystemApp = new VideoSystemController(
+		VideoSystem.getInstance(), new VideoSystemView()
+	);
+})();
+
+export default VideoSystemApp;
--- scripts/videoSystem/videoSystemController.js
+++ scripts/videoSystem/videoSystemController.js
@@ -2,10 +2,73 @@
 
 import VideoSystem from "./videoSystemModel.js";
 
-class VideoSystemController{
+class VideoSystemController {
     #videoSystem;
     #videoSystemView;
 
+    #loadVideoSystemObjects() {
+        // Categories
+        let cat1 = this.#videoSystem.getCategory("Fantasy");
+        let cat2 = this.#videoSystem.getCategory("Comedy");
+        let cat3 = this.#videoSystem.getCategory("Horror");
+        this.#videoSystem.addCategory(cat1);
+        this.#videoSystem.addCategory(cat2);
+        this.#videoSystem.addCategory(cat3);
+
+        // Productions
+        var movie1 = this.#videoSystem.getMovie("Black Adams", "Canada", "30/10/2022", "", "");
+        var movie2 = this.#videoSystem.getMovie("Free Guy", "United States", "13/08/2021", "", "");
+        var movie3 = this.#videoSystem.getMovie("Deadpool", "Canada", "12/02/2016", "", "");
+        var movie4 = this.#videoSystem.getMovie("The Invitation", "United States", "26/08/2022", "", "");
+        var movie5 = this.#videoSystem.getMovie("Nop", "United States", "22/07/2022", "", "");
+        var movie6 = this.#videoSystem.getMovie("De uskyldige", "Norway", "27/08/2021", "", "");
+        var serie1 = this.#videoSystem.getSerie("The Flash", "United States", "07/10/2014", "", "");
+        var serie2 = this.#videoSystem.getSerie("Wednesday", "United States", "23/11/2022", "", "");
+        var serie3 = this.#videoSystem.getSerie("Big Bang Theory", "United States", "24/09/2007", "", "");
+        var serie4 = this.#videoSystem.getSerie("The Fresh Prince of Bel-Air", "United States", "10/11/1990", "", "");
+        var serie5 = this.#videoSystem.getSerie("Ted Lasso", "United States", "14/08/2020", "", "");
+        var serie6 = this.#videoSystem.getSerie("Paranormal", "Egypt", "05/11/2020", "", "");
+        // Testear
+        // this.#videoSystem.addProduction(movie1, movie2, movie3);
+        this.#videoSystem.assignCategory(cat1, movie1, serie1, serie2, serie3);
+        this.#videoSystem.assignCategory(cat2, movie2, movie3, serie4, serie5);
+        this.#videoSystem.assignCategory(cat3, serie6, movie4, movie5, movie6);
+
+        // Directors
+        var director1 = this.#videoSystem.getDirector("Jaume", "Collet", "Serra", "00000001A", "23/03/1974");
+        var directorTemporal = this.#videoSystem.getDirector("Temporal", "Temporal", "", "88888888A", "23/03/1974");
+
+        // Actors
+        var actor1 = this.#videoSystem.getActor("Dwayne", "Johnson", "", "00001001A", "02/05/1972");
+        var actor2 = this.#videoSystem.getActor("Aldis", "Hodge", "", "00001002A", "20/09/1986");
+        var actor3 = this.#videoSystem.getActor("Ryan", "Reynolds", "", "00001003A", "23/10/1976");
+        var actorTemporal = this.#videoSystem.getActor("Temporal", "Temporal", "", "99999999A", "23/10/1976");
+
+        this.#videoSystem.assignDirector(director1, movie1);
+
+        this.#videoSystem.assignActor(actor1, movie1, movie2);
+        this.#videoSystem.assignActor(actor2, movie1);
+        this.#videoSystem.assignActor(actor3, movie2, movie3);
+        // Actor Temporal para seguir con el funcionamiento de nuestra app, una vez terminada se estableceran los actores y direcores correctamente
+        this.#videoSystem.assignActor(actorTemporal, movie4, movie5, movie6, serie1, serie2, serie3, serie4, serie5, serie6);
+        this.#videoSystem.assignDirector(director1, movie2, movie3 ,movie4, movie5, movie6, serie1, serie2, serie3, serie4, serie5, serie6);
+
+        // User
+        let adminUser = this.#videoSystem.getUser("Javier", "javiermb@gmail.com", "Abcd1234");
+        this.#videoSystem.addUser(adminUser);
+    }
+
+    constructor(model, view) {
+        console.log('Manager controller');
+        this.#videoSystem = model;
+        this.#videoSystemView = view;
+
+        this.onLoad();
+    }
+
+    onLoad = () => {
+        this.#loadVideoSystemObjects();
+    }
 }
 
 export default VideoSystemController;
