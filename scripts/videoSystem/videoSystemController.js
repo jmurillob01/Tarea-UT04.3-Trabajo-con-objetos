ca44cbcf2c277e720abd34364636ba85ae61a650
Javier Murillo <javiermb2302@gmail.com>
2023-02-12 12:21:39 +0100

    developing MVC
    Renamed videosystem

---
+++ scripts/videoSystem/videoSystemController.js
@@ -0,0 +1,11 @@
+"use strict";
+
+import VideoSystem from "./videoSystemModel.js";
+
+class VideoSystemController{
+    #videoSystem;
+    #videoSystemView;
+
+}
+
+export default VideoSystemController;
